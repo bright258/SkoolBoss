132bf45e3ebb4112ccc628b06a100b570fcb32ce
Bright Bassey <brightbassey2001@gmail.com>
2022-07-22 10:58:20 +0100

    Update settings.py

--- backend/skoolbus/skoolbus/settings.py
+++ backend/skoolbus/skoolbus/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-SECRET_KEY = 'django-insecure-xf)$fl7x-n$vy%uh_2$6+0gvx27e_6*c1%4^$y&_t_v+%60gsm'
+SECRET_KEY
 
 
 DEBUG = True
@@ -122,16 +122,16 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-COINBASE_COMMERCE_API_KEY ="8acfabb8-cd44-4096-8274-6b11a56a2aff"
+COINBASE_COMMERCE_API_KEY 
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-PAYSTACK_SECRET_KEY = "sk_test_4b55ff77139812eeff86146e0ce6ad5fb8d503ce"
+PAYSTACK_SECRET_KEY
 PAYSTACK_URL = "https://api.paystack.co"
 
 
 cloudinary.config(
-    cloud_name= 'dmjwzcjel',
-    api_key=  '166665833888589',
-    api_secret= 'FmuK9ZcteGr3UJg10wvrHGHOAAA'
+    cloud_name
+    api_key
+    api_secret
 )
