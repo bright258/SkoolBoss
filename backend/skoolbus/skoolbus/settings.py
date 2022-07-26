193edc5a31189b93d11617a45ed77ef94e8e8133
Bright Bassey <brightbassey2001@gmail.com>
2022-07-26 13:51:00 +0100

    another another commit

--- backend/skoolbus/skoolbus/settings.py
+++ backend/skoolbus/skoolbus/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import cloudinary
 import django_on_heroku
+import dj_database_url
 
 
 django_on_heroku.settings(locals())
@@ -13,13 +14,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xf)$fl7x-n$vy%uh_2$6+0gvx27e_6*c1%4^$y&_t_v+%60gsm'
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
 
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -68,15 +71,33 @@ WSGI_APPLICATION = 'skoolbus.wsgi.application'
 AUTH_USER_MODEL = 'core.User'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+    'default':{
+
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'',
+        'USER':"",
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':""
     }
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
+
+WHITENOISE_USE_FINDERS = True
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -126,6 +147,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestFilesStorage'
 COINBASE_COMMERCE_API_KEY ="8acfabb8-cd44-4096-8274-6b11a56a2aff"
 
 
